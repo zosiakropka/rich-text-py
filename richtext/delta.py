757a8169011f828b511cc7df1cb07363eeabd52f
Zosia Wiśniowolska <sobocinska.zosia@gmail.com>
2014-10-31 22:03:50 +0100

    Delta.push() implementation

--- richtext/delta.py
+++ richtext/delta.py
@@ -27,7 +27,35 @@ class Delta(object):
 	raise NotImplementedError()
 
     def push(self, newOp):
-	raise NotImplementedError()
+        index = len(self.ops)
+        lastOp = self.ops[index - 1] if index else {}
+        newOp = op.attributes.clone(newOp)
+        if iz.dictionary(lastOp):
+            if iz.number(newOp.get('delete')) and iz.number(lastOp.get('delete')):
+                self.ops[index - 1] = {'delete': lastOp.get('delete') + newOp.get('delete')}
+                return self
+        # Since it does not matter if we insert before or after deleting at the same index,
+        #  always prefer to insert first
+        if iz.number(lastOp.get('delete')) and iz.string(newOp.get('insert')) or iz.number(newOp.get('insert')):
+            index -= 1
+            lastOp = self.ops[index - 1]
+            if not iz.dictionary(lastOp):
+                self.ops = [newOp] + self.ops
+                return self
+
+        if iz.equal(newOp.get('attributes'), lastOp.get('attributes')):
+            if iz.string(newOp.get('insert')) and iz.string(lastOp.get('insert')):
+                self.ops[index - 1] = {'insert': lastOp['insert'] + newOp['insert']}
+                if iz.dictionary(newOp.get('attributes')):
+                    self.ops[index - 1]['attributes'] = newOp['attributes']
+                return self
+            elif iz.number(newOp.get('retain')) and iz.number(lastOp.get('retain')):
+                self.ops[index - 1] = {'retain': lastOp.get('retain') + newOp.get('retain')}
+                if iz.dictionary(newOp.get('attributes')):
+                    self.ops[index - 1]['attributes'] = newOp.get('attributes')
+                return self
+        self.ops = self.ops[0:index] + [newOp] + self.ops[index:-1]
+        return self
 
     def length(self):
 	raise NotImplementedError()
